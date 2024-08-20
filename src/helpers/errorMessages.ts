2df04e10d6abca3268386cbff25d6dde454bfcb0
Caio Queiroz <caio.cezar.dequeiroz@gmail.com>
2024-08-20 02:51:17 -0300

    feat: create error messages enum

---
+++ src/helpers/errorMessages.ts
@@ -0,0 +1,18 @@
+export const ERROR_MESSAGES = {
+  INVALID_TOKEN: 'Token de autorização inválido.',
+  MISSING_REQUIRED_PARAMETERS: 'Parâmetro obrigatório ausente.',
+  INVALID_CREDENTIALS: 'Credenciais Inválidas.',
+  USER_ALREADY_EXISTS: 'Usuário já registrado.',
+  USER_NOT_FOUND: 'Usuário não encontrado.',
+  ACCOUNT_NOT_FOUND: 'Conta não encontrada.',
+  INVALID_EMAIL: 'E-mail inválido.',
+  TIMEOUT: 'O tempo de espera para a operação foi excedido.',
+  TOO_MANY_REQUESTS:
+    'O acesso a esta conta foi temporariamente desativado devido a muitas tentativas de login com falha. Tente novamente mais tarde.',
+  EMAIL_ALREADY_IN_USE: 'O e-mail fornecido já está em uso por outra conta.',
+  INTERNAL_SERVER_ERROR: 'Erro interno no servidor.',
+  EMAIL_NOT_FOUND: 'E-mail não encontrado.',
+  USER_DISABLED: 'Conta do usuário desativada.',
+  PASSWORD_IS_DIFERENT_CONFIRM_PASSWORD:
+    'A senha e a confirmação de senha não conferem.',
+} as const;
