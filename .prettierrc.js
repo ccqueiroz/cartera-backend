c9c73904de9e289fec5107a6a1909b82bdddbb45
Caio Queiroz <caio.cezar.dequeiroz@gmail.com>
2024-08-20 02:49:40 -0300

    config: Create initial configs

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  singleQuote: true,
+  trailingComma: 'all',
+  jsxBracketSameLine: false,
+  arrowParens: 'always',
+  semi: true,
+  endOfLine: 'auto',
+  bracketSpacing: true
+};
