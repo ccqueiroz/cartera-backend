9369e3501d07f5c7f70f94284cf2323582448a1e
Caio Queiroz <caio.cezar.dequeiroz@gmail.com>
2024-10-22 01:44:22 -0300

    test: create test to create-person-user.usecase

---
+++ src/usecases/person_user/create-person-user.usecase.spec.ts
@@ -0,0 +1,124 @@
+import { EmailValidatorGateway } from '@/domain/Validators/EmailValidator/gateway/email-validator.gateway';
+import { CreatePersonUserUseCase } from './create-person-user.usecase';
+import { PersonUserGateway } from '@/domain/Person_User/gateway/person_user.gateway';
+import { ApiError } from '@/helpers/errors';
+import { convertOutputErrorToObject } from '@/helpers/convertOutputErrorToObject';
+import { ERROR_MESSAGES } from '@/helpers/errorMessages';
+
+let personUserGatewayMock: jest.Mocked<PersonUserGateway>;
+let emailValidatorGatewayMock: jest.Mocked<EmailValidatorGateway>;
+
+describe('Create Person User Usecase', () => {
+  let createPersonUserUseCase: CreatePersonUserUseCase;
+
+  beforeEach(() => {
+    personUserGatewayMock = {
+      getUserByEmail: jest.fn(),
+      createPersonUser: jest.fn(),
+    };
+
+    emailValidatorGatewayMock = {
+      validate: jest.fn(),
+    };
+
+    createPersonUserUseCase = CreatePersonUserUseCase.create({
+      personUserGateway: personUserGatewayMock,
+      emailValidatorGateway: emailValidatorGatewayMock,
+    });
+  });
+
+  it('should be create a instance of the CreatePersonUserUseCase class when will be use create method.', () => {
+    expect(createPersonUserUseCase).toBeInstanceOf(CreatePersonUserUseCase);
+  });
+
+  it('should call execute method when valid email and password are provided', async () => {
+    personUserGatewayMock.createPersonUser.mockResolvedValue({
+      id: 'P1fJ3',
+      fullName: 'john doe',
+    });
+
+    emailValidatorGatewayMock.validate.mockReturnValue(true);
+
+    const result = await createPersonUserUseCase.execute({
+      email: 'jonh.doe@example.com',
+      firstName: 'john',
+      lastName: 'doe',
+      userId: 'P1fJ3',
+      createdAt: '12121212121212',
+    });
+
+    expect(personUserGatewayMock.createPersonUser).toHaveBeenCalledWith({
+      email: 'jonh.doe@example.com',
+      firstName: 'john',
+      lastName: 'doe',
+      userId: 'P1fJ3',
+      createdAt: '12121212121212',
+    });
+
+    expect(result.data.fullName).toBe('john doe');
+    expect(result.data.id).toBe('P1fJ3');
+  });
+
+  it('should call execute method when not valid email are provided', async () => {
+    emailValidatorGatewayMock.validate.mockReturnValue(false);
+
+    const error = await createPersonUserUseCase
+      .execute({
+        email: '',
+        firstName: 'john',
+        lastName: 'doe',
+        userId: 'P1fJ3',
+        createdAt: '12121212121212',
+      })
+      .catch((er) => er);
+
+    expect(error).toBeInstanceOf(ApiError);
+
+    expect(convertOutputErrorToObject(error)).toEqual({
+      message: ERROR_MESSAGES.INVALID_EMAIL,
+      statusCode: 400,
+    });
+
+    expect(personUserGatewayMock.createPersonUser).not.toHaveBeenCalled();
+  });
+
+  it('should call execute method when the user already exist.', async () => {
+    emailValidatorGatewayMock.validate.mockReturnValue(true);
+
+    personUserGatewayMock.createPersonUser.mockResolvedValue({
+      id: 'P1fJ3',
+      fullName: 'john doe',
+    });
+
+    personUserGatewayMock.getUserByEmail.mockResolvedValue({
+      email: 'jonh.doe@example.com',
+      userId: 'P1fJ3',
+      id: 'P1fJ3',
+      firstName: 'jonh',
+      lastName: 'doe',
+      image: 'htpp://image',
+      fullName: 'jonh doe',
+      createdAt: '1212',
+      updatedAt: '1212',
+    });
+
+    const error = await createPersonUserUseCase
+      .execute({
+        email: 'jonh.doe@example.com',
+        firstName: 'john',
+        lastName: 'doe',
+        userId: 'P1fJ3',
+        createdAt: '12121212121212',
+      })
+      .catch((er) => er);
+
+    expect(error).toBeInstanceOf(ApiError);
+
+    expect(convertOutputErrorToObject(error)).toEqual({
+      message: ERROR_MESSAGES.EMAIL_ALREADY_IN_USE,
+      statusCode: 400,
+    });
+
+    expect(personUserGatewayMock.createPersonUser).not.toHaveBeenCalled();
+  });
+});
