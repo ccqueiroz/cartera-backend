c0c90dd62e9457c07eddecb283072feb63e12799
Caio Queiroz <caio.cezar.dequeiroz@gmail.com>
2024-12-15 02:56:53 -0300

    feat: create category entitie and tests

---
+++ src/domain/Category/entitie/category.entitie.ts
@@ -0,0 +1,27 @@
+import { CategoryDTO } from '../dtos/category.dto';
+
+export type CategoryProps = CategoryDTO;
+
+export class CategoryEntitie {
+  private constructor(private props: CategoryProps) {}
+
+  public static with(props: CategoryProps) {
+    return new CategoryEntitie(props);
+  }
+
+  public get id() {
+    return this.props.id;
+  }
+
+  public get description() {
+    return this.props.description;
+  }
+
+  public get createdAt() {
+    return this.props.createdAt;
+  }
+
+  public get updatedAt() {
+    return this.props.updatedAt;
+  }
+}
