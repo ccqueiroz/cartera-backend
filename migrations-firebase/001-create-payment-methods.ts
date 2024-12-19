4429ea09778b0b264a8546f32f02604b53816c0c
Caio Queiroz <caio.cezar.dequeiroz@gmail.com>
2024-12-19 01:29:11 -0300

    fix: adjust updatedAt attribute to set null.

--- migrations-firebase/001-create-payment-methods.ts
+++ migrations-firebase/001-create-payment-methods.ts
@@ -51,8 +51,8 @@ export default async function (db: Firestore) {
     await paymentMethodsRef.doc(method.id).set({
       id: method.id,
       description: method.description,
-      createdAt: `${new Date().getTime()}`,
-      updatedAt: '',
+      createdAt: new Date().getTime(),
+      updatedAt: null,
     });
     console.log(`Documento criado: ${method.description}`);
   }
