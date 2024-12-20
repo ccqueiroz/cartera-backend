ba1b0004544652dc75bd3a7c20847c847f95236f
Caio Queiroz <caio.cezar.dequeiroz@gmail.com>
2024-12-20 16:56:07 -0300

    chore: add InputDTO of the useCases

--- src/domain/Category/dtos/category.dto.ts
+++ src/domain/Category/dtos/category.dto.ts
@@ -1,6 +1,12 @@
 import { BaseDto } from '@/domain/dtos/baseDto.dto';
+import { CategoryType } from '../enums/category-type.enum';
 
 export type CategoryDTO = {
   id: string;
   description: string;
+  type: CategoryType;
 } & BaseDto;
+
+export type GetCategoriesInputDTO = { type?: CategoryType };
+
+export type GetCategoryByIdInputDTO = Pick<CategoryDTO, 'id'>;
