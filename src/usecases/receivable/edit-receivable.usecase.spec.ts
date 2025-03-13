934eb3110e19ea089d94ae5a2877649c49b950b3
Caio Queiroz <caio.cezar.dequeiroz@gmail.com>
2025-03-13 10:18:25 -0300

    chore: test of the cenary if dont to passed userId in request EditReceivableUseCase

---
+++ src/usecases/receivable/edit-receivable.usecase.spec.ts
@@ -0,0 +1,277 @@
+import { ReceivableGateway } from '@/domain/Receivable/gateway/receivable.gateway';
+import { ValidateCategoryPaymentMethodStatusUseCase } from '../validate_entities/validate-category-payment-method-status.usecase';
+import { CategoryGateway } from '@/domain/Category/gateway/category.gateway';
+import { PaymentMethodGateway } from '@/domain/Payment_Method/gateway/payment-method.gateway';
+import { PaymentStatusGateway } from '@/domain/Payment_Status/gateway/payment-status.gateway';
+import { EditReceivableUseCase } from './edit-receivable.usecase';
+import { ApiError } from '@/helpers/errors';
+import { convertOutputErrorToObject } from '@/helpers/convertOutputErrorToObject';
+import { ERROR_MESSAGES } from '@/helpers/errorMessages';
+
+let receivableGatewayMock: jest.Mocked<ReceivableGateway>;
+
+let editReceivableUseCase: EditReceivableUseCase;
+
+let categoryGatewayMock: jest.Mocked<CategoryGateway>;
+let paymentMethodGatewayMock: jest.Mocked<PaymentMethodGateway>;
+let paymentStatusGatewayMock: jest.Mocked<PaymentStatusGateway>;
+
+let validateCategoryPaymentMethodStatusUseCase: ValidateCategoryPaymentMethodStatusUseCase;
+
+const userIdMock = '1234567d';
+
+describe('Edit Receivable UseCase', () => {
+  beforeEach(() => {
+    receivableGatewayMock = {
+      createReceivable: jest.fn(),
+      deleteReceivable: jest.fn(),
+      getReceivableById: jest.fn(),
+      getReceivables: jest.fn(),
+      updateReceivable: jest.fn(),
+    };
+
+    categoryGatewayMock = {
+      getCategories: jest.fn(),
+      getCategoryById: jest.fn(),
+    };
+
+    paymentMethodGatewayMock = {
+      getPaymentMethodById: jest.fn(),
+      getPaymentMethods: jest.fn(),
+    };
+
+    paymentStatusGatewayMock = {
+      getPaymentStatus: jest.fn(),
+      getPaymentStatusById: jest.fn(),
+    };
+
+    validateCategoryPaymentMethodStatusUseCase =
+      ValidateCategoryPaymentMethodStatusUseCase.create({
+        categoryGateway: categoryGatewayMock,
+        paymentMethodGateway: paymentMethodGatewayMock,
+        paymentStatusGateway: paymentStatusGatewayMock,
+      });
+
+    editReceivableUseCase = EditReceivableUseCase.create({
+      receivableGateway: receivableGatewayMock,
+      validateCategoryPaymentMethodStatusService:
+        validateCategoryPaymentMethodStatusUseCase,
+    });
+  });
+
+  it('should be create a instance of the EditReceivableUseCase class when will be use create method.', () => {
+    expect(editReceivableUseCase).toBeInstanceOf(EditReceivableUseCase);
+  });
+
+  it('should edit a receivable when valid data and receivable id are provided', async () => {
+    const receivableData = {
+      id: 'e76176ad-c2d8-4526-95cb-1434d5149dd4',
+      categoryId: 'e76176ad-c2d8-4526-95cb-0440d0149dd4',
+      paymentMethodId: 'e76176ad-c2d8-4526-95cb-0440d0149dc6',
+      paymentStatusId: 'e76176ad-c2d8-4526-95cb-0440d0149d87',
+      personUserId: 'e76176ad-c2d8-4526-95cb-123456749d87',
+      userId: '1234567d-c2d8-4526-95cb-123456749d87',
+      descriptionReceivable: 'Test Receivable',
+      fixedReceivable: true,
+      receivableDate: new Date().getTime(),
+      icon: null,
+      amount: 100,
+      categoryDescription: 'Test Category',
+      paymentMethodDescription: 'Test Payment Method',
+      paymentStatusDescription: 'Paid',
+      receival: false,
+      receivalDate: null,
+      createdAt: new Date().getTime(),
+      updatedAt: null,
+    };
+
+    receivableGatewayMock.getReceivableById.mockResolvedValue({
+      ...receivableData,
+      updatedAt: null,
+    });
+
+    validateCategoryPaymentMethodStatusUseCase.execute = jest
+      .fn()
+      .mockResolvedValue(true);
+
+    receivableGatewayMock.updateReceivable.mockResolvedValue({
+      ...receivableData,
+      descriptionReceivable: 'Test Receivable updated',
+      updatedAt: new Date().getTime(),
+    });
+
+    const result = await editReceivableUseCase.execute({
+      receivableId: receivableData.id,
+      receivableData,
+      userId: userIdMock,
+    });
+
+    expect(result.data?.id).toBe(receivableData.id);
+    expect(receivableGatewayMock.updateReceivable).toHaveBeenCalledWith({
+      receivableId: receivableData.id,
+      receivableData,
+      userId: userIdMock,
+    });
+    expect(result.data?.updatedAt).toEqual(expect.any(Number));
+  });
+
+  it('should throw an error if the category, payment method, or payment status is invalid.', async () => {
+    const receivableData = {
+      id: 'e76176ad-c2d8-4526-95cb-1434d5149dd4',
+      categoryId: 'e76176ad-c2d8-4526-95cb-0440d0149dd4',
+      paymentMethodId: 'e76176ad-c2d8-4526-95cb-0440d0149dc6',
+      paymentStatusId: 'e76176ad-c2d8-4526-95cb-0440d0149d87',
+      personUserId: 'e76176ad-c2d8-4526-95cb-123456749d87',
+      userId: '1234567d-c2d8-4526-95cb-123456749d87',
+      descriptionReceivable: 'Test Receivable',
+      fixedReceivable: true,
+      receivableDate: new Date().getTime(),
+      icon: null,
+      amount: 100,
+      categoryDescription: 'Test Category',
+      paymentMethodDescription: 'Test Payment Method',
+      paymentStatusDescription: 'Paid',
+      receival: false,
+      receivalDate: null,
+      createdAt: new Date().getTime(),
+      updatedAt: null,
+    };
+
+    receivableGatewayMock.getReceivableById.mockResolvedValue({
+      ...receivableData,
+      updatedAt: null,
+    });
+
+    validateCategoryPaymentMethodStatusUseCase.execute = jest
+      .fn()
+      .mockResolvedValue(false);
+
+    const error = await editReceivableUseCase
+      .execute({
+        receivableId: receivableData.id,
+        receivableData,
+        userId: userIdMock,
+      })
+      .catch((err) => err);
+
+    expect(error).toBeInstanceOf(ApiError);
+    expect(convertOutputErrorToObject(error)).toEqual({
+      message: ERROR_MESSAGES.INVALID_CATEGORY_PAYMENT_METHOD_OR_PAYMENT_STATUS,
+      statusCode: 400,
+    });
+    expect(receivableGatewayMock.createReceivable).not.toHaveBeenCalled();
+  });
+
+  it('should throw an error if receivableId is not exist in data base.', async () => {
+    const receivableData = {
+      id: 'e76176ad-c2d8-4526-95cb-1434d5149dd4',
+      categoryId: 'e76176ad-c2d8-4526-95cb-0440d0149dd4',
+      paymentMethodId: 'e76176ad-c2d8-4526-95cb-0440d0149dc6',
+      paymentStatusId: 'e76176ad-c2d8-4526-95cb-0440d0149d87',
+      personUserId: 'e76176ad-c2d8-4526-95cb-123456749d87',
+      userId: '1234567d-c2d8-4526-95cb-123456749d87',
+      descriptionReceivable: 'Test Receivable',
+      fixedReceivable: true,
+      receivableDate: new Date().getTime(),
+      icon: null,
+      amount: 100,
+      categoryDescription: 'Test Category',
+      paymentMethodDescription: 'Test Payment Method',
+      paymentStatusDescription: 'Paid',
+      receival: false,
+      receivalDate: null,
+      createdAt: new Date().getTime(),
+      updatedAt: null,
+    };
+
+    receivableGatewayMock.getReceivableById.mockResolvedValue(null);
+
+    const error = await editReceivableUseCase
+      .execute({
+        receivableId: receivableData.id,
+        receivableData,
+        userId: userIdMock,
+      })
+      .catch((err) => err);
+
+    expect(error).toBeInstanceOf(ApiError);
+    expect(convertOutputErrorToObject(error)).toEqual({
+      message: ERROR_MESSAGES.RECEIVABLE_NOT_FOUND,
+      statusCode: 404,
+    });
+    expect(receivableGatewayMock.updateReceivable).not.toHaveBeenCalled();
+  });
+
+  it('should throw an error if receivableId is invalid', async () => {
+    const receivableData = {
+      id: 'e76176ad-c2d8-4526-95cb-1434d5149dd4',
+      categoryId: 'e76176ad-c2d8-4526-95cb-0440d0149dd4',
+      paymentMethodId: 'e76176ad-c2d8-4526-95cb-0440d0149dc6',
+      paymentStatusId: 'e76176ad-c2d8-4526-95cb-0440d0149d87',
+      personUserId: 'e76176ad-c2d8-4526-95cb-123456749d87',
+      userId: '1234567d-c2d8-4526-95cb-123456749d87',
+      descriptionReceivable: 'Test Receivable',
+      fixedReceivable: true,
+      receivableDate: new Date().getTime(),
+      icon: null,
+      amount: 100,
+      categoryDescription: 'Test Category',
+      paymentMethodDescription: 'Test Payment Method',
+      paymentStatusDescription: 'Paid',
+      receival: false,
+      receivalDate: null,
+      createdAt: new Date().getTime(),
+      updatedAt: null,
+    };
+
+    const error = await editReceivableUseCase
+      .execute({
+        receivableId: '',
+        receivableData,
+        userId: userIdMock,
+      })
+      .catch((err) => err);
+
+    expect(error).toBeInstanceOf(ApiError);
+    expect(convertOutputErrorToObject(error)).toEqual({
+      message: ERROR_MESSAGES.MISSING_REQUIRED_PARAMETERS,
+      statusCode: 400,
+    });
+    expect(receivableGatewayMock.updateReceivable).not.toHaveBeenCalled();
+  });
+
+  it('should be throw an error if the userId does not passed', async () => {
+    const receivableData = {
+      id: 'e76176ad-c2d8-4526-95cb-1434d5149dd4',
+      categoryId: 'e76176ad-c2d8-4526-95cb-0440d0149dd4',
+      paymentMethodId: 'e76176ad-c2d8-4526-95cb-0440d0149dc6',
+      paymentStatusId: 'e76176ad-c2d8-4526-95cb-0440d0149d87',
+      personUserId: 'e76176ad-c2d8-4526-95cb-123456749d87',
+      userId: '1234567d-c2d8-4526-95cb-123456749d87',
+      descriptionReceivable: 'Test Receivable',
+      fixedReceivable: true,
+      receivableDate: new Date().getTime(),
+      icon: null,
+      amount: 100,
+      categoryDescription: 'Test Category',
+      paymentMethodDescription: 'Test Payment Method',
+      paymentStatusDescription: 'Paid',
+      receival: false,
+      receivalDate: null,
+      createdAt: new Date().getTime(),
+      updatedAt: null,
+    };
+
+    const error = await editReceivableUseCase
+      .execute({ receivableId: '', receivableData, userId: '' })
+      .catch((err) => err);
+
+    expect(error).toBeInstanceOf(ApiError);
+    expect(convertOutputErrorToObject(error)).toEqual({
+      message: ERROR_MESSAGES.INVALID_CREDENTIALS,
+      statusCode: 401,
+    });
+
+    expect(receivableGatewayMock.updateReceivable).not.toHaveBeenCalled();
+  });
+});
