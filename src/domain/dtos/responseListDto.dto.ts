fe7aab83151e9d6e9e2a9f89b47e3a5a20588c29
Caio Queiroz <caio.cezar.dequeiroz@gmail.com>
2025-03-14 10:16:28 -0300

    chore: include ordering attribute in ResponseListDTO

--- src/domain/dtos/responseListDto.dto.ts
+++ src/domain/dtos/responseListDto.dto.ts
@@ -1,7 +1,10 @@
+import { SortOrder } from './listParamsDto.dto';
+
 export type ResponseListDTO<T> = {
   content: Array<T>;
   page: number;
   size: number;
   totalElements: number;
   totalPages: number;
+  ordering: { [K in keyof T]?: SortOrder } | null;
 };
