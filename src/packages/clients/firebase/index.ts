c8da6d0ec4e39daa729a786d7cd57d777995c434
Caio Queiroz <caio.cezar.dequeiroz@gmail.com>
2025-03-20 22:40:28 -0300

    chore: use only a sdk to management to project

--- src/packages/clients/firebase/index.ts
+++ src/packages/clients/firebase/index.ts
@@ -1,12 +1,8 @@
-import firebase from 'firebase';
-import firebaseConfig from './firebaseConfig.json';
-import admin, { ServiceAccount } from 'firebase-admin';
+import * as admin from 'firebase-admin';
 import serviceAcconutKey from './serviceAccountKey.json';
-// eslint-disable-next-line import/no-named-as-default-member
-const clientFireBase: firebase.app.App = firebase.initializeApp(firebaseConfig);
 
 const clientFireBaseAdmin: admin.app.App = admin.initializeApp({
-  credential: admin.credential.cert(serviceAcconutKey as ServiceAccount),
+  credential: admin.credential.cert(serviceAcconutKey as admin.ServiceAccount),
 });
 
-export { clientFireBase, clientFireBaseAdmin };
+export { clientFireBaseAdmin };
