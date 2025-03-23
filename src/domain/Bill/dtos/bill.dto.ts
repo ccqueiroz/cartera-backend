9548663568c475a9cd7757a6379c22b9cf4c1294
Caio Queiroz <caio.cezar.dequeiroz@gmail.com>
2025-03-23 18:31:35 -0300

    chore: add dtos that use in usecase and gateway

--- src/domain/Bill/dtos/bill.dto.ts
+++ src/domain/Bill/dtos/bill.dto.ts
@@ -48,3 +48,73 @@ export type BillDTO = {
   isShoppingListBill: boolean;
   shoppingListData?: ShoppingListData;
 } & BaseDto;
+
+export type SortByStatusBillsInputDTO =
+  | { paymentStatusId: string; categoryId?: never; paymentMethodId?: never }
+  | { paymentStatusId?: never; categoryId: string; paymentMethodId?: never }
+  | { paymentStatusId?: never; categoryId?: never; paymentMethodId: string };
+
+export type SortByBillTypeInputDTO = {
+  fixedBill?: boolean;
+  payOut?: boolean;
+  isShoppingListBill?: boolean;
+  isPaymentCardBill?: boolean;
+  amount?: number;
+};
+
+export type SearchByDateGetBillsInputDTO =
+  | { billDate: SearchByDate; payDate?: never }
+  | { payDate: SearchByDate; billDate?: never };
+
+export type ValuesOrderByGetBillsInputDTO =
+  | 'amount'
+  | 'billDate'
+  | 'payDate'
+  | 'categoryId'
+  | 'paymentMethodId'
+  | 'paymentStatusId'
+  | 'createdAt'
+  | 'updatedAt';
+
+export type OrderByField<T extends ValuesOrderByGetBillsInputDTO> = {
+  [K in T]: SortOrder;
+} & {
+  [K in Exclude<ValuesOrderByGetBillsInputDTO, T>]?: never;
+};
+
+export type OrderByGetBillsInputDTO =
+  | OrderByField<'amount'>
+  | OrderByField<'billDate'>
+  | OrderByField<'payDate'>
+  | OrderByField<'categoryId'>
+  | OrderByField<'paymentMethodId'>
+  | OrderByField<'paymentStatusId'>
+  | OrderByField<'createdAt'>
+  | OrderByField<'updatedAt'>;
+
+export type GetBillsInputDTO = Omit<
+  PaginationParams<SortByStatusBillsInputDTO>,
+  'searchByDate' | 'ordering'
+> & {
+  userId: string;
+  sortByBills?: SortByBillTypeInputDTO;
+  searchByDate?: SearchByDateGetBillsInputDTO;
+  ordering?: OrderByGetBillsInputDTO;
+};
+
+export type GetBillByIdInputDTO = Required<Pick<BillDTO, 'id' | 'userId'>>;
+
+export type CreateBillInputDTO = {
+  billData: Omit<BillDTO, 'id' | 'updatedAt'>;
+  userId: string;
+};
+
+export type CreateBillOutputDTO = Pick<BillDTO, 'id'>;
+
+export type EditBillInputDTO = {
+  billId: string;
+  billData: BillDTO;
+  userId: string;
+};
+
+export type DeleteBillInputDTO = Required<Pick<BillDTO, 'id' | 'userId'>>;
