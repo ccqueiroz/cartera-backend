9f855d5ef5806a820722e659879c44230c0c5c40
Caio Queiroz <caio.cezar.dequeiroz@gmail.com>
2025-03-26 23:40:45 -0300

    chore: includes applayPaginationHelpers, mergeSortHelpers and handleCanProgressToWritteOperation singletons

--- src/infra/helpers/index.ts
+++ src/infra/helpers/index.ts
@@ -1,5 +1,21 @@
+import { ApplyPaginationHelper } from './apply-pagination.helpers';
 import { CheckIfIsNecessaryCreateNewTokenHelpers } from './check-if-is-necessary-create-new-token.helpers';
+import { HandleCanProgressToWritteOperationHelper } from './handle-can-progress-to-writte-operation.helpers';
+import { MargeSortHelper } from './merge-sort.helpers';
 
 const checkIfIsNecessaryCreateNewTokenHelpers =
   new CheckIfIsNecessaryCreateNewTokenHelpers();
-export { checkIfIsNecessaryCreateNewTokenHelpers };
+
+const applayPaginationHelpers = new ApplyPaginationHelper();
+
+const mergeSortHelpers = new MargeSortHelper();
+
+const handleCanProgressToWritteOperation =
+  new HandleCanProgressToWritteOperationHelper();
+
+export {
+  checkIfIsNecessaryCreateNewTokenHelpers,
+  applayPaginationHelpers,
+  mergeSortHelpers,
+  handleCanProgressToWritteOperation,
+};
