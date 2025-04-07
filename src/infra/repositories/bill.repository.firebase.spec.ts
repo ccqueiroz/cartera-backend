f8e4d2132ae7b2a5cdb9ea73ad2b84462683b1c4
Caio Queiroz <caio.cezar.dequeiroz@gmail.com>
2025-04-07 16:39:41 -0300

    chore: create test to billsPayableMonth

--- src/infra/repositories/bill.repository.firebase.spec.ts
+++ src/infra/repositories/bill.repository.firebase.spec.ts
@@ -86,6 +86,129 @@ const billsItemsMock = [
   },
 ];
 
+const billsItemsSearchByPeriod = [
+  {
+    id: '24177d92-1aee-4479-859b-72f01c9ade24',
+    data: () => ({
+      personUserId: '06627d91-1aee-4479-859b-72f01c9ade24',
+      userId: 'b3e1c7f2-2d4e-48a5-a1f3-ef7c1e36d9b4',
+      descriptionBill: 'Supermercado',
+      fixedBill: false,
+      billDate: new Date('03-01-2025').getTime(),
+      payDate: new Date().getTime(),
+      payOut: true,
+      icon: null,
+      amount: 1200.0,
+      paymentStatusId: 'd5a2f9c1-3e6a-41b9-9e6d-5c8eaf39b1b2',
+      paymentStatusDescription: 'Pago',
+      categoryId: '7a3f4c8d-0e1b-43a9-91b5-4c7f6d9b2a6e',
+      categoryDescription: 'Supermercado',
+      paymentMethodId: 'g12c3e1b2-4a9e-4f6b-8d2e-3b7c6a1e5f9d',
+      paymentMethodDescription: 'Pix',
+      isPaymentCardBill: false,
+      isShoppingListBill: true,
+      createdAt: new Date('03-01-2025').getTime(),
+      updatedAt: null,
+    }),
+  },
+  {
+    id: '19582167-7jwr-1142-65cb-74d03d7az318',
+    data: () => ({
+      personUserId: '06627d91-1aee-4479-859b-72f01c9ade24',
+      userId: 'b3e1c7f2-2d4e-48a5-a1f3-ef7c1e36d9b4',
+      descriptionBill: 'Tim',
+      fixedBill: true,
+      billDate: new Date('03-01-2025').getTime(),
+      payDate: new Date().getTime(),
+      payOut: false,
+      icon: null,
+      amount: 60.0,
+      paymentStatusId: 'b78994ce-b7cb-4eed-9bdc-b7443358300c',
+      paymentStatusDescription: 'A pagar',
+      categoryId: '7a3f4c8d-0e1b-43a9-91b5-4c7f6d9b2a6e',
+      categoryDescription: 'Assinatura de Internet, Telefonia e Streamings',
+      paymentMethodId: '',
+      paymentMethodDescription: '',
+      isPaymentCardBill: false,
+      isShoppingListBill: false,
+      createdAt: new Date('03-01-2025').getTime(),
+      updatedAt: null,
+    }),
+  },
+  {
+    id: '48273619-3gtd-7831-92ad-83b18e3bp932',
+    data: () => ({
+      personUserId: '06627d91-1aee-4479-859b-72f01c9ade24',
+      userId: 'b3e1c7f2-2d4e-48a5-a1f3-ef7c1e36d9b4',
+      descriptionBill: 'Luz',
+      fixedBill: true,
+      billDate: new Date('03-12-2025').getTime(),
+      payDate: new Date().getTime(),
+      payOut: false,
+      icon: null,
+      amount: 120.0,
+      paymentStatusId: 'b78994ce-b7cb-4eed-9bdc-b7443358300c',
+      paymentStatusDescription: 'A pagar',
+      categoryId: '67815e45-44c3-415c-ba5f-5ab8998d7da6',
+      categoryDescription: 'Serviços e Utilidades Públicas',
+      paymentMethodId: '',
+      paymentMethodDescription: '',
+      isPaymentCardBill: false,
+      isShoppingListBill: false,
+      createdAt: new Date('03-12-2025').getTime(),
+      updatedAt: null,
+    }),
+  },
+  {
+    id: '48273619-3gtd-7831-92ad-83b18e3bp932',
+    data: () => ({
+      personUserId: '06627d91-1aee-4479-859b-72f01c9ade24',
+      userId: 'b3e1c7f2-2d4e-48a5-a1f3-ef7c1e36d9b4',
+      descriptionBill: 'Água',
+      fixedBill: true,
+      billDate: new Date('03-26-2025').getTime(),
+      payDate: new Date().getTime(),
+      payOut: false,
+      icon: null,
+      amount: 90.0,
+      paymentStatusId: 'b78994ce-b7cb-4eed-9bdc-b7443358300c',
+      paymentStatusDescription: 'A pagar',
+      categoryId: '67815e45-44c3-415c-ba5f-5ab8998d7da6',
+      categoryDescription: 'Serviços e Utilidades Públicas',
+      paymentMethodId: '',
+      paymentMethodDescription: '',
+      isPaymentCardBill: false,
+      isShoppingListBill: false,
+      createdAt: new Date('03-26-2025').getTime(),
+      updatedAt: null,
+    }),
+  },
+  {
+    id: '87263410-4qws-3409-81ab-63c09b8bk215',
+    data: () => ({
+      personUserId: '06627d91-1aee-4479-859b-72f01c9ade24',
+      userId: 'b3e1c7f2-2d4e-48a5-a1f3-ef7c1e36d9b4',
+      descriptionBill: 'Cartão Visa',
+      fixedBill: true,
+      billDate: new Date('04-01-2025').getTime(),
+      payDate: new Date().getTime(),
+      payOut: false,
+      icon: null,
+      amount: 5200.0,
+      paymentStatusId: 'b78994ce-b7cb-4eed-9bdc-b7443358300c',
+      paymentStatusDescription: 'A pagar',
+      categoryId: '67815e45-44c3-415c-ba5f-5ab8998d7da6',
+      categoryDescription: 'Despesa com Cartão de Crédito',
+      paymentMethodId: '',
+      paymentMethodDescription: '',
+      isPaymentCardBill: false,
+      isShoppingListBill: false,
+      createdAt: new Date('04-01-2025').getTime(),
+      updatedAt: null,
+    }),
+  },
+];
+
 const userIdMock = '1234567d';
 
 let billRepo: BillsRepositoryFirebase;
@@ -486,5 +609,92 @@ describe('Bill Repository Firebase', () => {
 
     expect(firestore.doc().get).toHaveBeenCalledTimes(1);
     expect(firestore.doc().delete).toHaveBeenCalledTimes(1);
+  });
+
+  it('should be call billsPayableMonth and return bills list that attend the requirements of the period and payout false', async () => {
+    firestore.where().orderBy().get.mockResolvedValueOnce({
+      docs: billsItemsSearchByPeriod,
+    });
+
+    const result = await billRepo.billsPayableMonth({
+      userId: userIdMock,
+      period: {
+        initialDate: new Date('03-01-2025').getTime(),
+        finalDate: new Date('03-31-2025').getTime(),
+      },
+    });
+
+    expect(result.length).toEqual(3);
+    expect(result[0].billDate).toEqual(new Date('03-01-2025').getTime());
+    expect(result[0].payOut).toBeFalsy();
+    expect(result[1].billDate).toEqual(new Date('03-12-2025').getTime());
+    expect(result[1].payOut).toBeFalsy();
+    expect(result[2].billDate).toEqual(new Date('03-26-2025').getTime());
+    expect(result[2].payOut).toBeFalsy();
+  });
+
+  it('should be throw an error when call billsPayableMonth if the userId do not exist in param', async () => {
+    const error = await billRepo
+      .billsPayableMonth({
+        userId: '',
+        period: {
+          initialDate: new Date('03-01-2025').getTime(),
+          finalDate: new Date('03-31-2025').getTime(),
+        },
+      })
+      .catch((err) => err);
+
+    expect(error).toBeInstanceOf(ApiError);
+    expect(convertOutputErrorToObject(error)).toEqual({
+      message: ERROR_MESSAGES.INVALID_CREDENTIALS,
+      statusCode: 401,
+    });
+  });
+
+  it('should be throw an error when call billsPayableMonth if the period not defined or initialDate or finalDate dont be defined/exist', async () => {
+    let error = await billRepo
+      .billsPayableMonth({
+        userId: userIdMock,
+        period: undefined as any,
+      })
+      .catch((err) => err);
+
+    expect(error).toBeInstanceOf(ApiError);
+    expect(convertOutputErrorToObject(error)).toEqual({
+      message: ERROR_MESSAGES.INVALID_PERIOD,
+      statusCode: 400,
+    });
+
+    error = await billRepo
+      .billsPayableMonth({
+        userId: userIdMock,
+        period: {
+          initialDate: new Date().getTime(),
+          finalDate: 'abs121212' as any,
+        },
+      })
+      .catch((err) => err);
+
+    expect(error).toBeInstanceOf(ApiError);
+    expect(convertOutputErrorToObject(error)).toEqual({
+      message: ERROR_MESSAGES.INVALID_PERIOD,
+      statusCode: 400,
+    });
+
+    error = await billRepo
+      .billsPayableMonth({
+        userId: userIdMock,
+        period: {
+          initialDate: undefined as any,
+          finalDate: new Date().getTime(),
+        },
+      })
+      .catch((err) => err);
+
+    expect(error).toBeInstanceOf(ApiError);
+    expect(convertOutputErrorToObject(error)).toEqual({
+      message: ERROR_MESSAGES.INVALID_PERIOD,
+      statusCode: 400,
+    });
   });
 });
