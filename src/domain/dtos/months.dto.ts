f7c3712406bd0ba107b271ab00264a7ce89af232
Caio Queiroz <caio.cezar.dequeiroz@gmail.com>
2025-04-08 23:40:13 -0300

    feature: create const to Months

---
+++ src/domain/dtos/months.dto.ts
@@ -0,0 +1,12 @@
+export const Months = {
+  JAN: 'JAN',
+  FEV: 'FEV',
+  MAR: 'MAR',
+  ABR: 'ABR',
+  MAI: 'MAI',
+  JUN: 'JUN',
+  JUL: 'JUL',
+  OUT: 'OUT',
+  NOV: 'NOV',
+  DEZ: 'DEZ',
+} as const;
