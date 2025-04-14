0d194dce10f34cd5a5fd29f766446917ed0ac8b6
Caio Queiroz <caio.cezar.dequeiroz@gmail.com>
2025-04-14 16:50:49 -0300

    chore: add constructor to received loggerError instance

--- src/infra/api/express/middlewares/error.middleware.ts
+++ src/infra/api/express/middlewares/error.middleware.ts
@@ -1,8 +1,11 @@
 import { Request, Response, NextFunction } from 'express';
 import { ApiError } from '../../../../helpers/errors/ApiErrors';
 import { ErrorMiddleware as ErrorMiddlewareInterface } from './middleware';
+import { LoggerGateway } from '@/domain/Helpers/gateway/logger.gateway';
 
 export class ErrorMiddleware implements ErrorMiddlewareInterface {
+  constructor(readonly loggerError: LoggerGateway['error']) {}
+
   public getHandler() {
     return async (
       error: Error & ApiError,
@@ -12,6 +15,8 @@ export class ErrorMiddleware implements ErrorMiddlewareInterface {
     ) => {
       const statusCode = error.statusCode ?? 500;
       const message = error.message;
+
+      this.loggerError(`{[STATUS]: ${statusCode}}: ${error.stack}`);
 
       response.status(statusCode).json({ message });
 
