da4a3bde8e9a8d365e33ad19a6351d413fa13ebc
Caio Queiroz <caio.cezar.dequeiroz@gmail.com>
2025-04-22 19:52:52 -0300

    feature: create and test person user schema validation

---
+++ src/infra/api/express/schema_validations/PersonUser/person-user.schema.ts
@@ -0,0 +1,33 @@
+import {
+  IsString,
+  IsDefined,
+  IsEmail,
+  MaxLength,
+} from '@/packages/clients/class-validator';
+
+export class EditPersonUserValidationDTO {
+  @IsDefined()
+  @IsString()
+  @MaxLength(60)
+  id!: string;
+
+  @IsDefined()
+  @IsEmail()
+  @MaxLength(50)
+  email!: string;
+
+  @IsDefined()
+  @IsString()
+  @MaxLength(50)
+  firstName!: string;
+
+  @IsDefined()
+  @IsString()
+  @MaxLength(50)
+  lastName!: string;
+
+  @IsDefined()
+  @IsString()
+  @MaxLength(50)
+  userId!: string;
+}
