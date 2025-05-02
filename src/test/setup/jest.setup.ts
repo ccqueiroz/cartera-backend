0c6faef9ffd8c1c314f9f1f392cb21e9172cdd67
Caio Queiroz <caio.cezar.dequeiroz@gmail.com>
2025-05-01 21:06:00 -0300

    chore: include mock to redis/client

--- src/test/setup/jest.setup.ts
+++ src/test/setup/jest.setup.ts
@@ -1,6 +1,14 @@
 import mockFirebaseAdmin from '../mocks/firebase-admin.mock';
+import { mockRedisClient } from '../mocks/redis.mock';
 
 jest.mock('firebase-admin', () => ({
   __esModule: true,
   ...mockFirebaseAdmin,
+}));
+
+jest.mock('@redis/client', () => ({
+  createClient: jest.fn(() => ({
+    __esModule: true,
+    ...mockRedisClient,
+  })),
 }));
