16d44902019c26546ac30b9f119f818228c42ba4
Caio Queiroz <caio.cezar.dequeiroz@gmail.com>
2025-05-03 23:14:23 -0300

    feature: create a Receivable Service Gateway

---
+++ src/domain/Receivable/gateway/receivable.service.gateway.ts
@@ -0,0 +1,34 @@
+import { ResponseListDTO } from '@/domain/dtos/responseListDto.dto';
+import {
+  CreateReceivableInputDTO,
+  CreateReceivableOutputDTO,
+  DeleteReceivableInputDTO,
+  EditReceivableInputDTO,
+  GetReceivableByIdInputDTO,
+  GetReceivablesInputDTO,
+  ReceivableDTO,
+} from '../dtos/receivable.dto';
+
+export interface ReceivableServiceGateway {
+  getReceivables(
+    input: GetReceivablesInputDTO,
+  ): Promise<ResponseListDTO<ReceivableDTO>>;
+
+  getReceivableById({
+    id,
+    userId,
+  }: GetReceivableByIdInputDTO): Promise<ReceivableDTO | null>;
+
+  createReceivable({
+    receivableData,
+    userId,
+  }: CreateReceivableInputDTO): Promise<CreateReceivableOutputDTO | null>;
+
+  updateReceivable({
+    receivableId,
+    receivableData,
+    userId,
+  }: EditReceivableInputDTO): Promise<ReceivableDTO>;
+
+  deleteReceivable({ id, userId }: DeleteReceivableInputDTO): Promise<void>;
+}
