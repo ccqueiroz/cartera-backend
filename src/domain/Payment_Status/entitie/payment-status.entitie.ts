96682ae21d8a74bd96972686d5df9e5382134131
Caio Queiroz <caio.queiroz@terceiro.rnp.br>
2025-07-15 12:31:58 -0300

    chore: create method to calc difference between invoice date and current date to say the invoice status.

--- src/domain/Payment_Status/entitie/payment-status.entitie.ts
+++ src/domain/Payment_Status/entitie/payment-status.entitie.ts
@@ -1,4 +1,5 @@
 import { PaymentStatusDTO } from '../dtos/payment-status.dto';
+import { PaymentStatusDescriptionEnum } from '../enum/payment-status-description.enum';
 
 export type PaymentStatusProps = PaymentStatusDTO;
 
@@ -7,6 +8,58 @@ export class PaymentStatusEntitie {
 
   public static with(props: PaymentStatusProps) {
     return new PaymentStatusEntitie(props);
+  }
+
+  private static calcDiffDaysBetweenInvoiceDateAndReferenceDate(
+    invoiceDate: number,
+    referenceDate: number,
+  ) {
+    const milleSecondsPerDay = 1000 * 60 * 60 * 24;
+
+    const diffInDays = Math.floor(
+      (invoiceDate - referenceDate) / milleSecondsPerDay,
+    );
+
+    return diffInDays;
+  }
+
+  public static setInvoiceStatus(
+    wasPaid: boolean,
+    invoiceDate: number,
+    referenceDate: number,
+    invoiceType: 'bill' | 'receivable',
+  ) {
+    if (wasPaid) {
+      return invoiceType === 'bill'
+        ? PaymentStatusDescriptionEnum.PAID
+        : PaymentStatusDescriptionEnum.RECEIVED;
+    } else {
+      const diffInDays = this.calcDiffDaysBetweenInvoiceDateAndReferenceDate(
+        invoiceDate,
+        referenceDate,
+      );
+      if (referenceDate > invoiceDate) {
+        return PaymentStatusDescriptionEnum.OVERDUE;
+      }
+
+      if (diffInDays > 5) {
+        return invoiceType === 'bill'
+          ? PaymentStatusDescriptionEnum.TO_PAY
+          : PaymentStatusDescriptionEnum.TO_RECEIVE;
+      }
+
+      if (diffInDays > 0 && diffInDays <= 5) {
+        return PaymentStatusDescriptionEnum.DUE_SOON;
+      }
+
+      if (diffInDays === 0) {
+        return PaymentStatusDescriptionEnum.DUE_DAY;
+      }
+
+      return invoiceType === 'bill'
+        ? PaymentStatusDescriptionEnum.TO_PAY
+        : PaymentStatusDescriptionEnum.TO_RECEIVE;
+    }
   }
 
   public get id() {
