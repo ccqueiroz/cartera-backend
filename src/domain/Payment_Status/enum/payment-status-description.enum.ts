4ec1ca6ae1cb69e9e80c35019efd81becc7c72a8
Caio Queiroz <caio.queiroz@terceiro.rnp.br>
2025-07-15 12:32:18 -0300

    feature: create PaymentStatusDescriptionEnum

---
+++ src/domain/Payment_Status/enum/payment-status-description.enum.ts
@@ -0,0 +1,9 @@
+export const PaymentStatusDescriptionEnum = {
+  PAID: 'PAID',
+  RECEIVED: 'RECEIVED',
+  TO_PAY: 'TO_PAY',
+  TO_RECEIVE: 'TO_RECEIVE',
+  DUE_SOON: 'DUE_SOON',
+  DUE_DAY: 'DUE_DAY',
+  OVERDUE: 'OVERDUE',
+} as const;
