bc46df9b6c4e977e99b517d50acc45fc533eb498
Caio Queiroz <caio.queiroz@terceiro.rnp.br>
2025-07-15 18:46:32 -0300

    fix: refactor payment method dto to comply with the new rules to payment method.

--- src/domain/Payment_Method/dtos/payment-method.dto.ts
+++ src/domain/Payment_Method/dtos/payment-method.dto.ts
@@ -1,6 +1,11 @@
 import { BaseDto } from '@/domain/dtos/baseDto.dto';
+import { PaymentMethodDescriptionEnum } from '../enums/payment-method-description.enum';
+
+export type PaymentMethodDescriptionEnumType =
+  (typeof PaymentMethodDescriptionEnum)[keyof typeof PaymentMethodDescriptionEnum];
 
 export type PaymentMethodDTO = {
   id: string;
   description: string;
+  descriptionEnum: PaymentMethodDescriptionEnumType;
 } & BaseDto;
