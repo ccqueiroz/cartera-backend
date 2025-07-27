35d8e39489d9e38e7c6bafa03e396f64c4fc9014
Caio Queiroz <caio.queiroz@terceiro.rnp.br>
2025-07-27 15:07:07 -0300

    chore: included ReceivablesSearchFilter, QueryReceivablesByFilterInputDTO, ReceivableCostFilterCategory and GetReceivablesCostsByCategoryAndByPeriod

--- src/domain/Receivable/dtos/receivable.dto.ts
+++ src/domain/Receivable/dtos/receivable.dto.ts
@@ -4,7 +4,11 @@ import { PersonUserEntitieDTO } from '@/domain/Person_User/dtos/person-user.dto'
 import { CategoryDTO } from '@/domain/Category/dtos/category.dto';
 import { PaymentMethodDTO } from '@/domain/Payment_Method/dtos/payment-method.dto';
 import { PaymentStatusDTO } from '@/domain/Payment_Status/dtos/payment-status.dto';
-import { PaginationParams, SortOrder } from '@/domain/dtos/listParamsDto.dto';
+import {
+  PaginationParams,
+  SearchByDate,
+  SortOrder,
+} from '@/domain/dtos/listParamsDto.dto';
 import { SortByStatusInputDTO } from '@/domain/Helpers/dtos/sort-by-status-input.dto';
 import { ReceivableSearchByDateDTO } from '@/domain/Helpers/dtos/search-by-date-input.dto';
 import { PaymentStatusDescriptionEnum } from '@/domain/Payment_Status/enum/payment-status-description.enum';
@@ -146,4 +150,32 @@ export type ReceivablesByMonthOutputDTO = {
   categoryDescriptionEnum: CategoryDescriptionEnum;
   categoryGroup: CategoryGroupEnum;
   status: (typeof PaymentStatusDescriptionEnum)[keyof typeof PaymentStatusDescriptionEnum];
+};
+
+export type ReceivablesSearchFilter = {
+  paid?: boolean;
+  category?: CategoryDescriptionEnum;
+  categoryGroup?: CategoryGroupEnum;
+  paymentMethod?: PaymentMethodDescriptionEnum;
+  paymentStatus?: PaymentStatusDescriptionEnum;
+  fixed?: boolean;
+};
+
+export type QueryReceivablesByFilterInputDTO = {
+  period: SearchByDate;
+  userId: string;
+  filters?: ReceivablesSearchFilter;
+};
+
+type ReceivableCostFilterCategory = {
+  categoryFilter: CategoryDescriptionEnum;
+  paid?: boolean;
+  paymentMethod?: PaymentMethodDescriptionEnum;
+  paymentStatus?: PaymentStatusDescriptionEnum;
+};
+
+export type GetReceivablesCostsByCategoryAndByPeriod = {
+  period: SearchByDate;
+  userId: string;
+  filters: ReceivableCostFilterCategory;
 };
