b71ea71e37ee963e110791a0c0e2723574f83fac
Caio Queiroz <caio.queiroz@terceiro.rnp.br>
2025-08-08 17:51:22 -0300

    chore: catch token from cookies in extract token

--- src/infra/api/express/middlewares/verify-token.middleware.ts
+++ src/infra/api/express/middlewares/verify-token.middleware.ts
@@ -3,26 +3,18 @@ import { Middleware } from './middleware';
 import { AuthGateway } from '@/domain/Auth/gateway/auth.gateway';
 import { ApiError } from '@/helpers/errors';
 import { ERROR_MESSAGES } from '@/helpers/errorMessages';
-import { CheckIfIsNecessaryCreateNewTokenGateWay } from '@/domain/Auth/helpers/check-if-is-necessary-create-new-token.gateway';
 
 export class VerifyTokenMiddleware implements Middleware {
-  private constructor(
-    private readonly authGateway: AuthGateway,
-    private readonly checkIfIsNecessaryCreateNewTokenGateway: CheckIfIsNecessaryCreateNewTokenGateWay,
-  ) {}
+  private constructor(private readonly authGateway: AuthGateway) {}
 
-  public static create(
-    authGateway: AuthGateway,
-    checkIfIsNecessaryCreateNewTokenGateway: CheckIfIsNecessaryCreateNewTokenGateWay,
-  ) {
-    return new VerifyTokenMiddleware(
-      authGateway,
-      checkIfIsNecessaryCreateNewTokenGateway,
-    );
+  public static create(authGateway: AuthGateway) {
+    return new VerifyTokenMiddleware(authGateway);
   }
 
   private extractToken(request: Request): string | null {
-    return request.headers.authorization?.split(' ')[1] || null;
+    const token = request.cookies?.['session'];
+
+    return token;
   }
 
   private handleError(error: unknown, next: NextFunction): void {
@@ -48,19 +40,6 @@ export class VerifyTokenMiddleware implements Middleware {
 
         if (!decodeToken)
           throw new ApiError(ERROR_MESSAGES.INTERNAL_SERVER_ERROR, 500);
-
-        if (
-          decodeToken &&
-          decodeToken?.expirationTime &&
-          !this.checkIfIsNecessaryCreateNewTokenGateway.execute(
-            +decodeToken?.expirationTime,
-          )
-        ) {
-          const newToken = await this.authGateway.createNewToken({
-            userId: decodeToken.userId,
-          });
-          response.setHeader('Authorization', `Baerer ${newToken}`);
-        }
 
         request.user_auth = { ...decodeToken };
         next();
