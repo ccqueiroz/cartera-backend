f8dac6d488ddf0f1cf612a5e83e952301ca23f80
Caio Queiroz <caio.queiroz@terceiro.rnp.br>
2025-08-11 15:39:45 -0300

    chore: add getCategoryByDescriptionEnum and getCategoryByGroup in category repository and category service

--- src/domain/Category/gateway/category.service.gateway.ts
+++ src/domain/Category/gateway/category.service.gateway.ts
@@ -7,4 +7,10 @@ import {
 export interface CategoryServiceGateway {
   getCategories(type: GetCategoriesInputDTO): Promise<Array<CategoryDTO>>;
   getCategoryById({ id }: GetCategoryByIdInputDTO): Promise<CategoryDTO | null>;
+  getCategoryByDescriptionEnum({
+    descriptionEnum,
+  }: Pick<CategoryDTO, 'descriptionEnum'>): Promise<CategoryDTO | null>;
+  getCategoryByGroup({
+    group,
+  }: Pick<CategoryDTO, 'group'>): Promise<Array<CategoryDTO>>;
 }
